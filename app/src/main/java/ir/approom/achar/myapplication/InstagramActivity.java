f50a5e8cf859338aa05a44b0f471a3267dd16c09
mehrdad faraji <mehrdadfaraji.cs@gmail.com>
2017-08-17 11:54:46 +0430

    Create instagram layout

---
+++ app/src/main/java/ir/approom/achar/myapplication/InstagramActivity.java
@@ -0,0 +1,22 @@
+package ir.approom.achar.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mehrdad on 8/17/17.
+ */
+
+public class InstagramActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instagram);
+
+    }
+}
